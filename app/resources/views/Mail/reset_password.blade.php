9dc8f258cb3384e82feb6a3d43259df23557cadc
taka <taka4677@ezweb.ne.jp>
2024-12-19 11:34:34 +0900

    update:パスワードリセット画面修正

---
+++ app/resources/views/Mail/reset_password.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>パスワードリセットのお知らせ</title>
+</head>
+
+<body>
+    <h1>パスワードリセットのご案内</h1>
+    <p>下記URLより、パスワードリセットをお願いいたします。</p>
+    <p>{{$reset_url}}</p>
+</body>
+
+</html>
